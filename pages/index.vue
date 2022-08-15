01d312645e11f6056eed4df9b2e8fb33285496ac
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2022-08-15 11:14:24 -0400

    🤗

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@ useHead({
     title: "hello",
     meta: [{
         name: 'viewport',
-        content: "height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi"
+        content: "height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
     }]
 })
 
